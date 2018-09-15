87e9eaae89af6ac5e04b987d8cd135c781c3c01e
Travis T <travistokuyama@gmail.com>
2018-09-15 14:58:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  # Your code here
+  calls.each do |call|
+    if call.length
 end
 
 def find_the_cheese# code an argument here
